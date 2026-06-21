76c6224762803ce5959b2ebd3bb7bd4eeda83454
krishna7572 <pollpeme001@gmail.com>
2026-06-21 13:20:33 +0530

    Create Settings.kt

---
+++ app/src/main/java/com/spica/app/Settings.kt
@@ -0,0 +1,14 @@
+package com.spica.app
+
+// ============================================
+//  SPICA SETTINGS - Yahan message edit karo
+// ============================================
+
+object Settings {
+
+    // Emergency SMS ka message (yahan badal sakte ho)
+    // {location} likhoge wahan automatically location aa jayega
+    var EMERGENCY_MESSAGE =
+        "EMERGENCY ALERT from SPICA. I need help immediately. My location: {location}"
+
+}
